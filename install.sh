6eca0061dc81d33b40552994f9862051f017c9ac
Bartosz Szczeciński <54596452+bartko4321@users.noreply.github.com>
2026-08-06 03:17:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -167,7 +167,7 @@ log_info "Instalacja pakietów głównych..."
 wait_for_apt
 PACKAGES_INSTALL=(
     # Przeglądarki komunikatory
-    google-chrome-stable brave-origin thunderbird thunderbird-l10n-pl telegram-desktop
+    google-chrome-stable brave-origin thunderbird telegram-desktop
     # Multimedia
     qbittorrent krita audacity gmic mixxx kdenlive handbrake soundconverter
     # Narzędzia systemowe
